c870c26d2b3d24bed0c185e67701d4d95dae6c15
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-14 18:03:13 +0100

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-
 import React from 'react';
 import logo from './logo.svg';
 import ServiceAvailbility from './features/service-availability/ServiceAvailability.js';
+import TodoList from './features/todo-list/TodoList.js';
+import Weather from './features/weather/Weather.js';
 import './App.css';
 
 function App() {
-    return ( 
+    return (
         <div className="App">
             <header className="App-header">
                 <img src={ logo }
-                    className="App-logo"
-                    alt="logo" />
+                     className="App-logo"
+                     alt="logo"/>
                 <ServiceAvailbility />
             </header>
-            <div className="App-content">
-
-            </div>
+            <main className="App-content">
+                <article className="App-content-base">
+                    <TodoList />
+                </article>
+                <aside className="App-content-aside">
+                    <Weather />
+                </aside>
+            </main>
         </div>
     );
 }
--- src/features/service-availability/ServiceAvailability.js
+++ src/features/service-availability/ServiceAvailability.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectStatus } from './service-availability-slice.js';
 import DisplayStatus from './DisplayStatus.js';
-
 import styles from './ServiceAvailability.module.css';
-
-
-
 
 export default function Counter() {
     const status = useSelector(selectStatus);
