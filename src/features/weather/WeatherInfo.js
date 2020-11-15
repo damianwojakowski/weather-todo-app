86816f904bcc7dec85a2ecfa73679ea86d221029
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-15 12:05:24 +0100

    add WeatherInfo hover and colors to temperature indicator

--- src/features/weather/WeatherInfo.js
+++ src/features/weather/WeatherInfo.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import classNames from "classnames";
+import ReactTooltip from 'react-tooltip';
+
+import './WeatherInfo.css';
+
 
 export default function WeatherInfo(props) {
-    return <div><p>Temperature: {props.weatherInfo.temp} <span>&#x2103;</span></p>
+    let temperatureClass = classNames({
+        'temperature': true,
+        'temperature-high': props.weatherInfo.temp > 25,
+    });
+
+    return (<div>
+        <ReactTooltip />
+        <p data-tip="This is temeprature in &#x2103;">Temperature:
+            <span className={temperatureClass}> {props.weatherInfo.temp}</span>
+            <span> &#x2103;</span>
+        </p>
         <p>Pressure: {props.weatherInfo.pressure} <span>hPa</span></p>
         <p>Humidity: {props.weatherInfo.humidity} <span>%</span></p>
         <p>Wind Speed: {props.weatherInfo.wind_speed} <span>meter/sec</span></p>
-    </div>
+    </div>);
 }
